676a5879c6a1b22fe34ce6c121b42d97a113bdfc
GUO-vocano <1605776459@qq.com>
2025-09-12 21:08:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+weight = input()
+kg_to_pd = 2.2046
+if weight.endswith('kg'):
+    kg = float(weight[:-2])
+    pd = kg * kg_to_pd
+    print(f"对应的英制重量为{pd:.3f}磅")
+elif weight.endswith('pd'):
+    pd = float(weight[:-2])
+    kg = pd / kg_to_pd
+    print(f"对应的公制重量为{kg:.3f}公斤") 
