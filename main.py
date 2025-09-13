b4ad531df72dc4b6f55117c1a49417fe8be7ef25
GUO-vocano <1605776459@qq.com>
2025-09-13 17:02:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if weight.endswith('kg'):
     print(f"对应的英制重量为{pd:.3f}磅")
 elif weight.endswith('pd'):
     pd = float(weight[:-2])
-    kg = pd / kg_to_pd
+    kg = pd *0.4535
     print(f"对应的公制重量为{kg:.3f}公斤") 
